77a222c34d7b712eb06f9e9688a9ace3e2e43144
dest-sandeep <sandeep@destinyitservices.com>
2024-04-24 23:22:51 +0530

    datatypes

---
+++ 01Basics/02datatypes.js
@@ -0,0 +1,20 @@
+  "use strict"
+//    treat all code at newer version
+// alrert(3+3) we are using node js not browsers
+
+
+let name = "sandeep";
+let age = 22;
+let isLoggedIn = false
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefiend =>
+// symbol => unique
+// object
+
+// console.log(typeof undefined) output undefiend
+// console.log(typeof null); output object
