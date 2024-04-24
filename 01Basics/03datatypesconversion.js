67838d4f86cdeef521f6c333d5554a80199f1a22
dest-sandeep <sandeep@destinyitservices.com>
2024-04-24 23:56:35 +0530

    datatypesconversion

---
+++ 01Basics/03datatypesconversion.js
@@ -0,0 +1,31 @@
+// let score = "33ad"
+// let score = "33"
+// let score = undefined
+// let score = NaN
+// let score = true
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+// let valueInNumber = Number(score);
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber)
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1 ; false => 0
+
+// let isLoggedIn = 1;
+// let isLoggedIn = ""
+// let isLoggedIn = "sandy"
+// let booleanIsLoogedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoogedIn)
+
+// 1=>true; 0=> false 
+// ""=> false
+// "hitesh"=> true
+
+let someNumber = 33
+let stringNumber = String(someNumber)
+console.log(stringNumber)
+console.log(typeof stringNumber)
