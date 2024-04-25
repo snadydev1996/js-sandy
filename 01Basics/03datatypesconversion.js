971dc9940b04acc2f21755c5f03dd367bb7495dd
dest-sandeep <sandeep@destinyitservices.com>
2024-04-25 11:29:31 +0530

    operations

--- 01Basics/03datatypesconversion.js
+++ 01Basics/03datatypesconversion.js
@@ -25,7 +25,26 @@
 // ""=> false
 // "hitesh"=> true
 
-let someNumber = 33
-let stringNumber = String(someNumber)
-console.log(stringNumber)
-console.log(typeof stringNumber)
+// let someNumber = 33
+// let stringNumber = String(someNumber)
+// console.log(stringNumber)
+// console.log(typeof stringNumber)
+
+// *************** Operations ***************
+
+// let value = 3;
+// let negValue = -value
+// console.log(negValue)
+
+// console.log(2+2)
+// console.log(2-2)
+// console.log(2*2)
+// console.log(2**2)
+// console.log(2/3)
+// console.log(2%3)
+
+let str1 = "hello"
+let str2 = " hitesh"
+
+let str3 = str1 + str2
+console.log(str3);
